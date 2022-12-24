e734ffe193d20a4537c7f35eb54adf220b375ab9
co-bby <nkumsagoefredrick@gmail.com>
2022-12-23 23:21:00 -0500

    feat: set up register controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,12 @@
+const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes');
+const { BadRequestError } = require('../errors');
+
 const register = async (req, res) => {
-  res.send('register user');
+  const { name, email, password } = req.body;
+
+  const user = await User.create({ ...req.body });
+  res.status(StatusCodes.CREATED).json({ user });
 };
 
 const login = async (req, res) => {
