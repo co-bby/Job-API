54fdb8293cd441c7412a82a5ed69cc1ed6c59bbd
co-bby <nkumsagoefredrick@gmail.com>
2022-12-25 23:39:59 -0500

    feat: add mongoose pre middleware to hash password

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -21,5 +22,10 @@ const userSchema = new mongoose.Schema({
     required: [true, 'Please provide password'],
     minLength: 6,
   },
+});
+
+userSchema.pre('save', async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 module.exports = mongoose.model('User', userSchema);
