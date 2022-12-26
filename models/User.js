ddcb3fdff09a852e24f45918d679b488f78915d6
co-bby <nkumsagoefredrick@gmail.com>
2022-12-25 23:07:18 -0500

    feat: add hashing to password

--- models/User.js
+++ models/User.js
@@ -20,7 +20,6 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please provide password'],
     minLength: 6,
-    maxLength: 12,
   },
 });
 module.exports = mongoose.model('User', userSchema);
